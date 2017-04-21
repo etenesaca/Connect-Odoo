c8930f7fa1ed61402c9462039efb694171ed0aa4
Edgar Tenesaca <edgartenesaca@gmail.com>
2017-04-21 13:55:48 -0500

    Implemented basic ORM methods

--- openerp.js
+++ openerp.js
@@ -1,100 +1,167 @@
+#!/usr/bin/env node
+"use strict";
+
 var xmlrpc = require('xmlrpc');
 var hoag = require('hoag');
 
-var _TestConnection = function (host, port, callback) {
-	var rpc_db = xmlrpc.createClient({
-    	host: host,
-        port: port,
-        path:'/xmlrpc/db'
+var getDbList = function (protocol, host, port) {
+    return new Promise(function (resolve, reject) {        
+        var rpc_db = xmlrpc.createClient({
+            host: host,
+            port: port,
+            path:'/xmlrpc/db'
+        });
+        rpc_db.methodCall('list', [], function (err, result) {
+            if (err) return reject(err);
+            resolve(result);
+        });
     });
-    rpc_db.methodCall('list', [], callback);
 };
 
-OpenERP = function(host, port){
-	var url = 'http://' + host + ':' + port;
-	this.url = url
-	this.host = host;
-	this.port = port;
+var OpenERP = function(protocol, host, port, db){
+    this.protocol = protocol;
+    this.host = host;
+    this.port = port;
+    this.url = hoag.strings.formatJson("{protocol}://{host}:{port}", {protocol: protocol, host: host, port:port});
+    if (db){
+        this.db = db;
+        this.url = this.url + '/' + this.db;        
+    }
 
-	// COMMON
+    // COMMON
     this.rpc_common = xmlrpc.createClient({
-    	host: this.host,
+        host: this.host,
         port: this.port,
         path:'/xmlrpc/common'
     });
     // OBJECT
     this.rpc_object = xmlrpc.createClient({
-    	host: this.host,
+        host: this.host,
         port: this.port,
         path:'/xmlrpc/object'
     });
     // DATABASE
     this.rpc_db = xmlrpc.createClient({
-        host: this.host,
-        port: this.port,
-        path:'/xmlrpc/object'
+        host: host,
+        port: port,
+        path:'/xmlrpc/db'
     });
     // REPORT
     this.rpc_report = xmlrpc.createClient({
-    	host: this.host,
+        host: this.host,
         port: this.port,
         path:'/xmlrpc/report'
-    });
-    _TestConnection(this.host, this.port, function(err, db_list){        
-        if (err)
-        	hoag.logger.error('Error Connecting to OpenERP Server: ' + url, true);
-        else
-        	hoag.logger.info('Connecting sucessful to OpenERP Server: ' + url, true);
-	});
-};
-
-// Test connection
-var testConnection = function(callback){
-    _TestConnection(this.host, this.port, callback);
-};
-
-// Check database is available
-var checkDb = function(db_name, callback){
-    _TestConnection(this.host, this.port, function (err, db_list) {
-        var db_found = false;
-        for (var i = 0; i < db_list.length; i++) {
-            if (db_name == db_list[i])
-                db_found = true;
-        }
-        callback(db_found);
     });
 };
 
 // List Databases
-var listDatabases = function(callback){
-    _TestConnection(this.host, this.port, callback);
+var listDatabases = () => getDbList(this.protocol, this.host, this.port);
+// Test Connection
+var testConnection = function () {
+    var protocol = this.protocol;
+    var host = this.host;
+    var port = this.port;
+    return new Promise(function (resolve, reject) {
+        getDbList(protocol, host, port)
+        .then(result => {
+            resolve(result && true || false)
+        })
+        .catch((err) => resolve(false))
+    })
 };
-
-// Method to set Database
-var connect = function(db){
-    this.db = db;
-    this.url = 'http://' + this.host + ':' + this.port + '/' + this.db;
+// Check database is available
+var checkDb = function(db_name){
+    return new Promise(function (resolve, reject) {
+        this.listDatabases()
+        .then(dbList => {
+            var db_found = false;
+            for (var i = 0; i < db_list.length; i++) {
+                if (db_name == db_list[i])
+                    db_found = true;
+            }
+            resolve(db_found);
+        })
+        .catch(err => reject(err))
+    });
 };
-
 //Method to login user
-var login = function(username, password, callback){	
-	var url = this.url;
-	this.username = username;
-	this.password = password;			
-	this.rpc_common.methodCall('login', [this.db, this.username, this.password], function (err, uid) {
-		if (err)
-			hoag.logger.error('Bad login: ' + this.url, true);
-		else
-			hoag.logger.info("Successful login: '" + username + "' IN " + url, hoag.logger_type);
-		callback(err, uid);
-	});
+var login = function(username, password, db){   
+    if (db)
+        this.db = db;
+    var db = this.db;
+    this.username = username;
+    this.password = password;           
+    return new Promise(function (resolve, reject) {
+        this.rpc_common.methodCall('login', [db, username, password], function (err, uid) {
+            if (err) return reject(err)
+            this.uid = uid; // SET USER ID
+            resolve(uid);
+        });
+    })
+};
+// Set Credentials Exists Previous Login Information
+var setUID = function(username, uid, password){ 
+    this.username = username;
+    this.uid = uid;
+    this.password = password;   
+};
+
+/*
+EXECUTE ORM METHODS
+*/
+var execObject = function(model, method){
+    var rpc_object = this.rpc_object;
+    var params = [this.db, this.uid, this.password]
+    for (var i in arguments)
+        params.push(arguments[i]);
+    return new Promise(function (resolve, reject) {
+        rpc_object.methodCall('execute', params, function (err, result) {
+            if (err) return reject(err)
+            resolve(result);
+        });
+    });
+};
+var search = function(model, args, offset, limit, order, context){
+    offset = offset && !isNaN(parseFloat(offset)) && isFinite(offset) && parseInt(offset) || 0;
+    limit = limit && !isNaN(parseFloat(limit)) && isFinite(limit) && parseInt(limit) || null;
+    order = order || '';
+    context = context || {};
+    return this.execObject(model, 'search', args, offset, limit, order, context)
+};
+var create = function(model, values, context){
+    context = context || {};
+    return this.execObject(model, 'create', values, context)
+};
+var write = function(model, ids, values, context){
+    ids = typeof ids == 'number' && [ids] || ids;
+    context = context || {};
+    return this.execObject(model, 'write', ids, values, context)
+};
+var read = function(model, ids, fields, context){
+    fields = fields || [];
+    context = context || {};
+    return this.execObject(model, 'read', ids, fields, context)
+};
+var unlink = function(model, ids, values){
+    ids = typeof ids == 'number' && [ids] || ids;
+    context = context || {};
+    return this.execObject(model, 'unlink', ids, context)
 };
 
 OpenERP.prototype = {
-    connect: connect,
-    login: login,
     testConnection: testConnection,
+    listDatabases: listDatabases,
+    login: login,
+    setUID: setUID,
     checkDb: checkDb,
-    listDatabases: listDatabases
-};
-module.exports = OpenERP
+    // ORM METHODS
+    execObject: execObject,
+    search: search,
+    read: read,
+    create: create,
+    write: write,
+    unlink: unlink
+}
+module.exports = OpenERP;
+module.exports.testConnection = getDbList;
+module.exports.getDbList = getDbList
